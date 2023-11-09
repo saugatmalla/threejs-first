ed61b5f2bddfdf6e13e71a25f9c629a9237d3b45
Saugat <saugat013@gmail.com>
2023-11-09 13:46:24 -0500

    feat: restrict camera rotation, add some nit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+const COLORS = {
+    red: '#C63B2F',
+    green: '#C5E250',
+    blue: '#0000ff',
+    white: '#acacac',
+    brown: '#463220',
+}
+
 const canvas = document.querySelector('canvas.webgl');
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(COLORS.white);
 
 //object
 const landGeometry = new THREE.PlaneGeometry(4, 3);
-const landMaterial = new THREE.MeshBasicMaterial({ color: "#5c4826", side: THREE.DoubleSide });
+const landMaterial = new THREE.MeshBasicMaterial({ color: COLORS.brown, side: THREE.DoubleSide });
 const landMesh = new THREE.Mesh(landGeometry, landMaterial);
 landMesh.rotateX(Math.PI * 0.5);
 
@@ -24,10 +33,10 @@ const boxPositions = [
 function generateBoxes() {
     const boxes = [];
     for (let i = 0; i < boxPositions.length; i++) {
-        const height = Math.random() * 1.5;
+        const height = Math.random() * 1 + 0.3;
         const box = new THREE.Mesh(
             new THREE.BoxGeometry(0.2, height, 0.2),
-            new THREE.MeshBasicMaterial({ color: "#ff0000" })
+            new THREE.MeshBasicMaterial({ color: COLORS.red })
         );
         box.position.set(boxPositions[i][0], boxPositions[i][1] + height * 0.5 + 0.01, boxPositions[i][2]);
         boxes.push(box);
@@ -61,16 +70,16 @@ window.addEventListener('mousemove', (event) => {
 window.addEventListener('click', () => {
     if (currentIntersect) {
         if (selectedIntersect) {
-            selectedIntersect.object.material.color.set('#ff0000')
+            selectedIntersect.object.material.color.set(COLORS.red)
             if (selectedIntersect.object === currentIntersect.object) {
                 selectedIntersect = null;
             } else {
                 selectedIntersect = currentIntersect;
-                selectedIntersect.object.material.color.set('#00ff00')
+                selectedIntersect.object.material.color.set(COLORS.green)
             }
         } else {
             selectedIntersect = currentIntersect;
-            selectedIntersect.object.material.color.set('#00ff00')
+            selectedIntersect.object.material.color.set(COLORS.green)
         }
     }
 })
@@ -90,14 +99,16 @@ function handleRaycasterHover() {
 
 function handleMouseHover() {
     if (intersects.length) {
-        if (currentIntersect === null) {
-            console.log('mouse enter');
-        }
+        // if (currentIntersect === null) {
+        //     console.log('mouse enter');
+        // }
+        document.body.style.cursor = 'pointer';
         currentIntersect = intersects[0];
     } else {
-        if (currentIntersect) {
-            console.log('mouse leave');
-        }
+        // if (currentIntersect) {
+        //     console.log('mouse leave');
+        // }
+        document.body.style.cursor = 'auto';
         currentIntersect = null;
     }
 }
@@ -114,10 +125,12 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 
 const controls = new OrbitControls(camera, canvas);
+controls.minDistance = 2;
+controls.maxDistance = 5;
+controls.maxPolarAngle = Math.PI * 0.5;
 controls.enableDamping = true;
 
 const clock = new THREE.Clock();
-
 
 function animate() {
 
